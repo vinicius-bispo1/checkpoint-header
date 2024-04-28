af941e4def12ab921b48fca017f4b4173afae4f7
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-04-28 19:39:58 -0300

    adicionei um h2

--- src/page/inicio/Inicio.jsx
+++ src/page/inicio/Inicio.jsx
@@ -2,13 +2,21 @@ import Header from '../../components/Header'
 import * as S from "./styled"
 
 
+
 export default function Inicio() {
   return (
     <S.Main>
       <Header/>
-      <section>
-        <h2>inicio</h2>
-      </section>
+      <S.Container>
+        <S.BoxInicio>
+          <div>
+            <h2>Bom dia</h2>
+          <h1>Olá meu nome e vinicius sou dev front-end</h1>
+          <p>Não é a linguagem de programação que define o programador, mas sim sua lógica.</p>
+         </div>
+          <img src="https://png.pngtree.com/png-clipart/20231019/original/pngtree-user-profile-avatar-png-image_13369991.png" alt="" />
+        </S.BoxInicio>
+      </S.Container>
     </S.Main>
   )
 }
